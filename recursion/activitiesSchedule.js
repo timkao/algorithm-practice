320675c89080b9dfca8eff4431af8707db49fa50
Tim Kao <pakuya.kao@gmail.com>
2018-08-24 09:33:22 -0400

    add activities schedule optimism

--- recursion/activitiesSchedule.js
+++ recursion/activitiesSchedule.js
@@ -1,6 +1,7 @@
-const example = [[2, 7], [1, 8], [3, 6], [2, 4], [5, 9]]
+const example = [[2, 7], [1, 8], [3, 6], [2, 4], [5, 9], [0, 1]]
 
 
+// go through all the possibilities
 function schedule(activities, schedules = []) {
   if (activities.length === 0) {
     return schedules.length;
@@ -26,3 +27,36 @@ function schedule(activities, schedules = []) {
 }
 
 console.log(schedule(example))
+
+
+// optimize with Math
+function scheduleOpt(activities, schedule = []) {
+
+  if (activities.length === 0) {
+    console.log(schedule);
+    return schedule.length;
+  }
+
+
+  // sort activites
+  const sortedActs = activities.sort((a, b) => {
+    return a[1] > b[1]
+  }).concat()
+
+  // take the first one, update schedule
+  const currentAct = sortedActs[0];
+  const newSchedule = schedule.concat([currentAct]);
+  // remove overlaps
+  const actsWithoutOverlap = sortedActs.filter(act => {
+    return currentAct[0] > act[1] || currentAct[1] < act[0]
+  })
+
+  // not take the first one, use old schedule
+
+  // remove first one
+  const actsWithoutCurrent = sortedActs.slice(1, sortedActs.length);
+
+  return Math.max(scheduleOpt(actsWithoutOverlap, newSchedule), scheduleOpt(actsWithoutCurrent, schedule))
+}
+
+console.log(scheduleOpt(example));
