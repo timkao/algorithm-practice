edaf548ff281c22b08fc4509bdc977eb5b70f9aa
Tim Kao <pakuya.kao@gmail.com>
2018-09-11 21:01:25 -0400

    add masterMind

---
+++ chapter-Sixteen/masterMind.js
@@ -0,0 +1,31 @@
+function masterMind(ans, guess) {
+  let hit = 0;
+  let seudo = 0;
+  const ansObj = {};
+  let restGuess = [];
+  for (var i = 0; i < ans.length; i++) {
+    if (ans[i] === guess[i]) {
+      hit++
+    } else {
+      if (ansObj[ans[i]]) {
+        ansObj[ans[i]]++
+      } else {
+        ansObj[ans[i]] = 1;
+      }
+      restGuess.push(guess[i])
+    }
+  }
+
+  for (var j = 0; j < restGuess.length; j++) {
+    if (ansObj[restGuess[j]] > 0) {
+      ansObj[restGuess[j]]--
+      seudo++
+    }
+  }
+
+  return [hit, seudo];
+
+}
+
+console.log(masterMind('RGBY', 'GGRR'));
+console.log(masterMind('GBYY', 'YGYY'));
