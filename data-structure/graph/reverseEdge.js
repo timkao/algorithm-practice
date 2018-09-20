5cff812e5c2062dc57caa0776dc071ff9459b468
Tim Kao <pakuya.kao@gmail.com>
2018-09-19 21:29:55 -0400

    reverse edges

--- data-structure/graph/reverseEdge.js
+++ data-structure/graph/reverseEdge.js
@@ -1,4 +1,5 @@
 const { Vertex } = require('./findShortestPath');
+const template = require('./dftTemplate')
 
 const one = new Vertex(1)
 const two = new Vertex(2)
@@ -6,27 +7,37 @@ const three = new Vertex(3)
 const four = new Vertex(4)
 const five = new Vertex(5)
 const six = new Vertex(6)
+const seven = new Vertex(7)
+const eight = new Vertex(8)
 
 one.neighbors.push(two)
-one.neighbors.push(five)
-one.neighbors.push(six)
-two.neighbors.push(one)
 two.neighbors.push(three)
+two.neighbors.push(four)
 two.neighbors.push(five)
-three.neighbors.push(two)
-three.neighbors.push(four)
-four.neighbors.push(three)
-four.neighbors.push(five)
+three.neighbors.push(one)
+four.neighbors.push(one)
+four.neighbors.push(six)
+four.neighbors.push(eight)
+five.neighbors.push(six)
+six.neighbors.push(seven)
+seven.neighbors.push(five)
+eight.neighbors.push(six)
 
-five.neighbors.push(two)
-five.neighbors.push(four)
-six.neighbors.push(one)
-
-/* assume all vertex has unique integer value */
+const processed = []
+const discovered = []
+const entryTime = []
+const exitTime = []
+const parent = []
+const copied = []
+let time = 0
 
 function processVertexEarly(vertex) {
   console.log('-------- start processing --------')
   console.log(`This is ${vertex.value}`);
+  if (copied[vertex.value] === undefined) {
+    console.log(`${vertex.value} is copied`)
+    copied[vertex.value] = new Vertex(vertex.value)
+  }
   console.log('-------- start processing --------\n')
 }
 
@@ -34,6 +45,12 @@ function processEdge(vertex, neighbor) {
   console.log('----- Do whatever we want to vertex and the neighbor ------')
   console.log(`The vertex is ${vertex.value}`)
   console.log(`The neighbor is ${neighbor.value}`)
+
+  if (copied[neighbor.value] === undefined) {
+    copied[neighbor.value] = new Vertex(neighbor.value)
+  }
+  copied[neighbor.value].neighbors.push(copied[vertex.value]);
+
   console.log('----- Do whatever we want to vertex and the neighbor ------\n')
 }
 
@@ -43,15 +60,9 @@ function processVertexLate(vertex) {
   console.log('----- Post Processed ------\n')
 }
 
-const processed = []
-const discovered = []
-const entryTime = []
-const exitTime = []
-const parent = []
-let time = 0
 
 // 越先發現，越晚做完
-function dftTemplate(graph) {
+function dfs(graph) {
 
   processVertexEarly(graph)
   time += 1 // take a record before recursion
@@ -67,7 +78,7 @@ function dftTemplate(graph) {
       currNeighbor.parent = graph
       parent[currNeighbor.value] = graph.value
       processEdge(graph, currNeighbor)
-      dftTemplate(currNeighbor)
+      dfs(currNeighbor)
     } else if (!processed[currNeighbor.value]) {
       processEdge(graph, currNeighbor)
     }
@@ -81,7 +92,10 @@ function dftTemplate(graph) {
 
 }
 
-module.exports = {
-  dftTemplate,
-  discovered,
+function reverseEdges(graph) {
+  dfs(graph)
+  return copied[graph.value]
 }
+
+const newGraph = reverseEdges(one)
+template.dftTemplate(newGraph)
