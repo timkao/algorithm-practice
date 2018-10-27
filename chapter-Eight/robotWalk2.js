a3fd34636b1f03f2e93cb3a2c73d3692665430a1
Tim Kao <pakuya.kao@gmail.com>
2018-10-27 11:46:30 -0400

    add memoization

--- chapter-Eight/robotWalk2.js
+++ chapter-Eight/robotWalk2.js
@@ -14,22 +14,35 @@ const exampleGrid2 = [
   [true, true, true]
 ];
 
-function walksFromBeginning(grid, start = [0, 0], path = [[0, 0]]) {
+function walksFromBeginning(grid, start = [0, 0], path = [[0, 0]], failedCells = []) {
   const currRow = start[0]
   const currCol = start[1]
   if (currRow === grid.length - 1 && currCol === grid[0].length - 1) {
     return path
   }
+
+  /* the existence of failed Cells is for Memoization */
+  failedCells.forEach(failedCell => {
+    if (failedCell[0] === currRow && failedCell[1] === currCol) {
+      return false
+    }
+  })
+
   if (currRow + 1 <= grid.length - 1 && grid[currRow + 1][currCol] === true) {
     const downPath = path.concat([[currRow + 1, currCol]])
-    const downResult = walksFromBeginning(grid, [currRow + 1, currCol], downPath)
-    if (downResult !== undefined) return downResult
+    const downResult = walksFromBeginning(grid, [currRow + 1, currCol], downPath, failedCells)
+    if (downResult !== false) return downResult
   }
   if (currCol + 1 <= grid[0].length - 1 && grid[currRow][currCol + 1] === true) {
     const rightPath = path.concat([[currRow, currCol + 1]])
-    const rightResult = walksFromBeginning(grid, [currRow, currCol + 1], rightPath)
-    if (rightResult !== undefined) return rightResult
+    const rightResult = walksFromBeginning(grid, [currRow, currCol + 1], rightPath, failedCells)
+    if (rightResult !== false) return rightResult
   }
+
+  /* the existence of failed Cells is for Memoization */
+  failedCells.push([currRow, currCol])
+
+  return false
 }
 
 console.log(walksFromBeginning(exampleGrid))
