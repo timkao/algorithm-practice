4aeeb723471c6e745ebd0e89f774dd109d469df5
Tim Kao <pakuya.kao@gmail.com>
2018-11-03 13:22:54 -0400

    add spiral and hanoi tower

--- chapter-Eight/powerOfHanoi2.js
+++ chapter-Eight/powerOfHanoi2.js
@@ -1,14 +1,14 @@
- const orig = [5, 4, 3, 2, 1]
+ const orig = [3, 2, 1]
  const buff = []
  const dest = []
 
- function moveDisks(disks, origin, destination, buffer) {
-   if (disks > 0) {
-     moveDisks(disks - 1, origin, buffer, destination)
-     destination.push(origin.pop())
-     moveDisks(disks - 1, buffer, destination, origin)
-   }
- }
+function moveDisks(disks, origin, destination, buffer) {
+  if (disks > 0) {
+    moveDisks(disks - 1, origin, buffer, destination)
+    destination.push(origin.pop())
+    moveDisks(disks - 1, buffer, destination, origin)
+  }
+}
 
 moveDisks(orig.length, orig, dest, buff)
 console.log(orig)
--- recursion/spiral.js
+++ recursion/spiral.js
@@ -6,25 +6,25 @@ const arr = [
 ];
 
 function spiral(matrix, sr, sc, er, ec) {
-if (sr <= er) {
-for (var i = sr; i <= ec; i++) {
-console.log(matrix[sr][i]);
-}
-for (var j = sr + 1; j <= er; j++) {
-console.log(matrix[j][ec]);
-}
-for (var k = ec - 1; k >= sc; k--) {
-console.log(matrix[er][k]);
-}
-for (var l = er - 1; l > sr; l-- ) {
-console.log(matrix[l][sc]);
-}
-sr++
-sc++
-er--
-ec--
-spiral(matrix, sr, sc, er, ec);
-}
+  if (sr <= er) {
+  for (var i = sr; i <= ec; i++) {
+    console.log(matrix[sr][i]);
+  }
+  for (var j = sr + 1; j <= er; j++) {
+    console.log(matrix[j][ec]);
+  }
+  for (var k = ec - 1; k >= sc; k--) {
+    console.log(matrix[er][k]);
+  }
+  for (var l = er - 1; l > sr; l-- ) {
+    console.log(matrix[l][sc]);
+  }
+    sr++
+    sc++
+    er--
+    ec--
+    spiral(matrix, sr, sc, er, ec);
+  }
 }
 
 spiral(arr, 0, 0, arr.length - 1, arr.length - 1);
