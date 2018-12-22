99b49ed4a934e56454e5607499ae581801d67fb0
Tim Kao <pakuya.kao@gmail.com>
2018-12-22 14:11:48 -0500

    added optmized selection rank algorithm for unique values

--- chapter-Seventeen/smallestK.js
+++ chapter-Seventeen/smallestK.js
@@ -20,6 +20,7 @@ function smallestKUnique(nums, target) {
   if (left.length < target - 1) return smallestKUnique(right, target - left.length - 1)
 }
 
+assert.equal(smallestKUnique(ex, 0), null)
 assert.equal(smallestKUnique(ex, 1), -9)
 assert.equal(smallestKUnique(ex, 2), 0)
 assert.equal(smallestKUnique(ex, 3), 3)
@@ -30,3 +31,69 @@ assert.equal(smallestKUnique(ex, 7), 55)
 assert.equal(smallestKUnique(ex, 8), 73)
 assert.equal(smallestKUnique(ex, 9), 98)
 assert.equal(smallestKUnique(ex, 10), 100)
+
+function selectionRankUnique(nums, target) {
+  if (target <= 0 || target > nums.length) throw new Error('invalid inputs')
+  const threshold = findThreshold(nums, target - 1)
+  const smallest = []
+  let count = 0
+  for (var i = 0; i < nums.length; i++) {
+    const currVal = nums[i]
+    if (currVal <= threshold) {
+      smallest[count] = currVal
+      count += 1
+    }
+  }
+  return smallest
+}
+
+function findThreshold(nums, rank) {
+  return findRank(nums, 0, nums.length - 1, rank)
+}
+
+function findRank(nums, left, right, rank) {
+  const pivot = nums[left + Math.floor((right - left) / 2)]
+  const leftEnd = partition(nums, left, right, pivot)
+  const leftSize = leftEnd - left + 1
+  if (rank === leftSize - 1) {
+    return findMax(nums, left, leftEnd)
+  } else if (rank < leftSize) {
+    return findRank(nums, left, leftEnd, rank)
+  } else {
+    return findRank(nums, leftEnd + 1, right, rank - leftSize)
+  }
+}
+
+function partition(nums, left, right, pivot) {
+  while (left <= right) {
+    if (nums[left] > pivot) {
+      swap(nums, left, right)
+      right -= 1
+    } else if (nums[right] <= pivot) {
+      swap(nums, left, right)
+      left += 1
+    } else {
+      right -= 1
+      left += 1
+    }
+  }
+  return left - 1
+}
+
+function swap(arr, left, right) {
+  const temp = arr[left]
+  arr[left] = arr[right]
+  arr[right] = temp
+}
+
+function findMax(arr, left, right) {
+  let result = Number.NEGATIVE_INFINITY
+  for (var i = left; i <= right; i++) {
+    if (arr[i] > result ) {
+      result = arr[i]
+    }
+  }
+  return result
+}
+
+console.log(selectionRankUnique(ex, 7))
