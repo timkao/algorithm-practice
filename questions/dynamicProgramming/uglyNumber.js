acd0e51add9603b223467dc384abac235b2bff6e
Tim Kao <pakuya.kao@gmail.com>
2019-01-26 13:44:06 -0500

    ugly number true optimazation

--- questions/dynamicProgramming/uglyNumber.js
+++ questions/dynamicProgramming/uglyNumber.js
@@ -76,3 +76,28 @@ function swap(arr, p1, p2) {
   arr[p1] = arr[p2]
   arr[p2] = temp
 }
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nthUglyNumberOpt = function(n) {
+  var res = [1];
+  var i2=0, i3=0, i5=0;
+  while(res.length <n) {
+    var m2 = res[i2]*2;
+    var m3 = res[i3]*3;
+    var m5 = res[i5]*5;
+    var mn = Math.min(m2, Math.min(m3, m5));
+    if(mn === m2) i2++;
+    if(mn === m3) i3++;
+    if(mn === m5) i5++;
+    res.push(mn);
+  }
+  return res[res.length-1];
+};
