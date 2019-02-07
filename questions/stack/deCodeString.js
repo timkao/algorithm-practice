fcdd2d27c16d3398006bbf26d969ea495d32d878
Tim Kao <pakuya.kao@gmail.com>
2019-02-06 22:42:52 -0500

    refactor decode string

--- questions/stack/deCodeString.js
+++ questions/stack/deCodeString.js
@@ -1,15 +1,9 @@
-/**
- * @param {string} s
- * @return {string}
- */
 var decodeString = function(s) {
   const stack = []
   for (let i = 0; i < s.length; i++) {
       const currChar = s[i]
       if (isNumStr(currChar)) {
-          if (stack.length === 0) {
-              stack.push(currChar)
-          } else if (isNumStr(stack[stack.length - 1])) {
+          if (isNumStr(stack[stack.length - 1])) {
               stack[stack.length - 1] += currChar
           } else {
               stack.push(currChar)
@@ -21,10 +15,7 @@ var decodeString = function(s) {
         }
         stack.pop()
         let times = Number(stack.pop())
-        while (times > 0) {
-            stack.push(temp)
-            times -= 1
-        }
+        stack.push(temp.repeat(times))
       } else {
           stack.push(currChar)
       }
