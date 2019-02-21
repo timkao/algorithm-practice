65e147b18d4993f59a11e4fd603044f7288bf589
Tim Kao <pakuya.kao@gmail.com>
2019-02-21 11:43:13 -0500

    refactor flip game

--- questions/backTracking/flipGame.js
+++ questions/backTracking/flipGame.js
@@ -1,37 +1,23 @@
-function flipGame(str, count = 0, memo = [{}, {}]) {
-  const turns = count % 2
-  if (memo[turns][str] !== undefined) return memo[turns][str]
-  const moves = generateMove(str)
-  if (moves.length === 0) return turns === 0
-  for (let i = 0; i < moves.length; i++) {
-    const currMove = moves[i]
-    const nextStr = modifiedStr(str, currMove)
-    if (flipGame(nextStr, count + 1, memo)) {
-      memo[turns][str] = true
-      return true
+var canWin = function(s) {
+  const arr = s.split('')
+  return findWin(arr)
+};
+
+function findWin(arr, memo = {}) {
+  const str = arr.join('')
+  if (memo[str] !== undefined) return memo[str]
+  for (let i = 0; i < arr.length - 1; i++) {
+    const curSign = arr[i]
+    const nextSign = arr[i + 1]
+    if (curSign === '+' && nextSign === '+') {
+        arr[i] = '-'
+        arr[i + 1] = '-'
+        const temp = !findWin(arr, memo)
+        arr[i] = '+'
+        arr[i + 1] = '+'
+        if (temp) return true
     }
   }
-  memo[turns][str] = false
+  memo[str] = false
   return false
-}
-
-function generateMoves(str) {
-  const result = []
-  for (let i = 0; i < str.length - 1; i++) {
-    const neighbor = i + 1
-    if (str[i] === '+' && str[neighbor] === '+') result.push(i)
-  }
-  return result
-}
-
-function modifiedStr(str, move) {
-  let result = ''
-  for (let i = 0; i < str.length; i++) {
-    if ( i !== move && i !== move + 1){
-      result += str[i]
-    } else {
-      result += '-'
-    }
-  }
-  return result
 }
