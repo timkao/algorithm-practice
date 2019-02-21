a3848c3668707dd554c68f909ec8f2953db853a9
Tim Kao <pakuya.kao@gmail.com>
2019-02-21 14:31:54 -0500

    added evaluation division bfs solution

--- questions/graph/evaluationDivision.js
+++ questions/graph/evaluationDivision.js
@@ -39,3 +39,41 @@ function transition(map, from, to, visited = {}) {
   }
   return -1
 }
+
+
+var calcEquationBfs = function(equations, values, queries) {
+  const transMap = createTransmap(equations, values)
+  return queries.map(query => {
+      return transition(transMap, query[0], query[1])
+  })
+
+  function transition(map, from, to, visited = {}, parent = {}) {
+      if (map[from] === undefined || map[to] === undefined) return -1
+      if (from === to) return 1
+      visited[from] = true
+      const queue = []
+      queue.push(from)
+      while(queue.length > 0) {
+          const curEle = queue.shift()
+          if (curEle === to) {
+              return 1 / calculate(map, parent, from, to)
+          }
+          const children = Object.keys(map[curEle])
+          for (let i = 0; i < children.length; i++) {
+              const child = children[i]
+              if (visited[child] !== true) {
+                  visited[child] = true
+                  parent[child] = curEle
+                  queue.push(child)
+              }
+          }
+      }
+      return -1
+  }
+
+  function calculate(map, parent, start, end) {
+      if (parent[end] === start) return map[end][start]
+      return map[end][parent[end]] * calculate(map, parent, start, parent[end])
+  }
+
+};
