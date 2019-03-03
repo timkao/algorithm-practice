d6a6c6342387a9bac79605c82d3e7b6fe80798b2
Tim Kao <pakuya.kao@gmail.com>
2019-03-03 09:41:44 +0800

    two way to do preorder traversal, recursion and stack

---
+++ questions/tree/preorderTraversal.js
@@ -0,0 +1,25 @@
+var preorderTraversal = function(root, result = []) {
+  if (root === null) return result
+  result.push(root.val)
+  preorderTraversal(root.left, result)
+  preorderTraversal(root.right, result)
+  return result
+};
+
+var traversalWithStack = function(root) {
+  const result = []
+  const stack = []
+  stack.push(root)
+
+  while (stack.length > 0) {
+    const curNode = stack.pop()
+    if (curNode !== null) {
+      result.push(curNode.val)
+      stack.push(curNode.right)
+      stack.push(curNode.left)
+    }
+  }
+  return result
+};
+
+
