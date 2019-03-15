1d00174a327bf2866904379a3b3388664698196c
Tim Kao <pakuya.kao@gmail.com>
2019-03-15 18:29:06 -0400

    added explanation

--- questions/multiPointers/minSubArrayLen.js
+++ questions/multiPointers/minSubArrayLen.js
@@ -31,14 +31,14 @@ var minSubArrayLenOpt = function(s, nums) {
   let ans = Number.POSITIVE_INFINITY
   let slow = 0
   let sum = 0
-  for (let fast = 0; fast < nums.length; fast++) {
+  for (let fast = 0; fast < nums.length; fast++) { // move the fast pointer to increase the sum
       sum += nums[fast]
-      localAns += 1
-      while (sum >= s) {
+      localAns += 1 // since we move the fast pointer by 1, the local answer is increased by 1
+      while (sum >= s) { // start from the first qualified position. Also, whenever we reach the target, we check
           ans = Math.min(ans, localAns)
           sum -= nums[slow]
-          slow += 1
-          localAns -= 1
+          slow += 1 // move the slow pointer to decrease the sum
+          localAns -= 1 // since we move the slow pointer by one, the local answer is decreased by 1
       }
   }
   return ans === Number.POSITIVE_INFINITY ? 0 : ans
