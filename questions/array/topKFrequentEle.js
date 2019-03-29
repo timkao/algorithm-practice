df8a1a6cd519536d1d43abd36fe347a477a72831
Tim Kao <pakuya.kao@gmail.com>
2019-03-29 18:57:58 -0400

    added optmization for top k frequent element. use frequency as index

--- questions/array/topKFrequentEle.js
+++ questions/array/topKFrequentEle.js
@@ -30,3 +30,45 @@ function createMap(nums) {
   })
   return result
 }
+
+var topKFrequentOpt = function(nums, k) {
+  const map = new Map();
+  const freqMap = [];
+  let result = [];
+
+  // This builds of map of num: frequency. After looping, it looks like this
+  // {5: 2, 1: 4, 4: 2, 3: 1, 6: 1}
+  for (const num of nums) {
+      if (!map.has(num)) {
+          map.set(num, 0);
+      }
+      map.set(num, map.get(num) + 1);
+  }
+
+  // Now convert that map into an array of arrays, where the array index represents the frequency.
+  // This is the "key insight" to solving this problem w/o sorting as array indexes are naturally in order
+  // After this loop, freqMap looks like this
+  // [,[3,6],[5,4],,[1]]
+  map.forEach((occurrence, int) => {
+      const set = typeof freqMap[occurrence] === 'undefined' ? [] : freqMap[occurrence];
+      set.push(int);
+      freqMap[occurrence] = set;
+  });
+
+  // Since the most frequent entries are in the tail of the array, loop from the end and fill result
+  // until it has k elements. After loop, result looks like this
+  // [1, 5, 4]
+  for (let i = freqMap.length - 1; i >= 0; i--) {
+      if (typeof freqMap[i] !== 'undefined') {
+          freqMap[i].forEach((e) => result.push(e));
+      }
+      if (result.length > k) {
+          result = result.slice(0, k);
+      }
+      if (result.length === k) {
+          break;
+      }
+  }
+
+  return result;
+};
