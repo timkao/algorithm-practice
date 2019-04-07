0c2671373379d6e913a64ca6fbe07e70849c8b97
Tim Kao <pakuya.kao@gmail.com>
2019-04-07 12:12:56 -0400

    remove two pointers techniques

--- questions/multiPointers/removeDuplicates.js
+++ questions/multiPointers/removeDuplicates.js
@@ -18,3 +18,24 @@ var removeDuplicates = function(nums) {
   }
   return modifier + 1
 };
+
+
+var removeDuplicates = function(nums) {
+    if (nums.length <= 2) return nums.length
+    let reachLimit = false
+    let confirm = 0
+
+    for (let i = 1; i < nums.length; i++) {
+        const curNum = nums[i]
+        const prevNum = nums[i - 1]
+        if (curNum === prevNum && !reachLimit) {
+            reachLimit = true
+            confirm += 1
+        } else if (curNum !== prevNum) {
+            if (reachLimit) reachLimit = false
+            confirm += 1
+        }
+        nums[confirm] = curNum
+    }
+    return confirm + 1
+};
